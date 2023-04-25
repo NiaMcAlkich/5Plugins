6ee7ccee0b43a376b81c51fae64552e1764f65ea
Nia McAlkich <niamcalkich@gmail.com>
2023-04-25 15:47:00 -0400

    Made 3/5 work so far

---
+++ index.js
@@ -0,0 +1,13 @@
+const supervillains = require('supervillains');
+
+var randoSV = supervillains.random();
+console.log(randoSV);
+
+var oneLinerJoke = require('one-liner-joke');
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke);
+
+const lucky = require('lucky');
+
+console.log(lucky);
